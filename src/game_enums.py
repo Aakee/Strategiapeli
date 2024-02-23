2cddc74442f744bbec4848ba03a3f5f9f1fc765d
Akseli Konttas <aakeli.kaakeli@gmail.com>
2024-02-23 23:12:55 +0200

    Cleaned game_enums.py

--- src/game_enums.py
+++ src/game_enums.py
@@ -49,21 +49,22 @@ class SkillType(Enum):
     Different skills
     '''
     #Skills which affect moving.
-    LEVITATE = 1    # Character can levitate for instance over gaps
-    GHOST = 2       # Character can pass walls and enemy players (but not land on them)
-    SNEAK = 3       # Character can pass enemy players
+    LEVITATE    = 1    # Character can levitate for instance over gaps
+    GHOST       = 2    # Character can pass walls and enemy players (but not land on them)
+    SNEAK       = 3    # Character can pass enemy players
     
     #Skills which affect in combat.
-    CAMOUFLAGE = 11
-    BODYGUARD = 12
-    MIRACLE = 13
-    SNIPER = 14
+    CAMOUFLAGE  = 11
+    BODYGUARD   = 12
+    MIRACLE     = 13
+    SNIPER      = 14
     
     #Passive skills which affect outside of combat
-    REST = 21       # Character gains HP at the start of each turn.
+    REST        = 21   # Character gains HP at the start of each turn.
     
     #Active skills which affect outside of combat (must be activated)
-    HEAL = 31       # Character can heal other characters (and themselves).
-    RAISEDEF = 33   # Character raises defensive stats for nearby allies until start of next turn
-    RAISERNG = 34   # Character raises range for nearby allies until start of next turn
-    WISH = 39       # Another character can move second time
+    HEAL        = 31   # Character can heal other characters (and themselves).
+    RAISEDEF    = 33   # Character raises defensive stats for nearby allies until start of next turn
+    RAISERNG    = 34   # Character raises range for nearby allies until start of next turn
+    WISH        = 39   # Another character can move second time
+    
