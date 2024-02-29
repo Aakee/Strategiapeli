219f527237a4607adebe8c4f2440995eb8d450dd
Akseli Konttas <aakeli.kaakeli@gmail.com>
2024-02-29 18:34:31 +0200

    Modified ai functions

--- src/game_ai.py
+++ src/game_ai.py
@@ -6,7 +6,7 @@ import random
 import skill
 from game_enums import CharacterClass, PlayerColor
 from game_errors import IllegalMoveException
-from move import Move
+from move import Move, Value
 
 import copy
 import math
@@ -202,16 +202,14 @@ def calculate_enemy_threat(char,square):
         
     return value
 
-# Approximate values how well each character class fares against each other character class.
-# Scale: from -10 (very bad) to 10 (very good). 0 is neutral.
 BASE_VALUES = {
-    CharacterClass.ARCHER   : 10,
-    CharacterClass.ASSASSIN : 5,
-    CharacterClass.CLERIC   : 100,
-    CharacterClass.KNIGHT   : 10,
-    CharacterClass.MAGE     : 10,
-    CharacterClass.VALKYRIE : 20,
-    CharacterClass.TESTCHAR : 10,
+    CharacterClass.ARCHER   : 1,
+    CharacterClass.ASSASSIN : 1,
+    CharacterClass.CLERIC   : 10,
+    CharacterClass.KNIGHT   : 1,
+    CharacterClass.MAGE     : 1,
+    CharacterClass.VALKYRIE : 2,
+    CharacterClass.TESTCHAR : 1,
 }
 
 PREFER_ALLY_COMPANY = {
@@ -234,6 +232,8 @@ PREFER_ENEMY_COMPANY = {
     CharacterClass.TESTCHAR : 5,
 }
 
+# Approximate values how well each character class fares against each other character class.
+# Scale: from -10 (very bad) to 10 (very good). 0 is neutral.
 ADVANTAGES = {
     CharacterClass.TESTCHAR: {
         CharacterClass.ARCHER   : 0,
@@ -245,57 +245,57 @@ ADVANTAGES = {
         CharacterClass.TESTCHAR : 0,
     },
     CharacterClass.ARCHER: {
-        CharacterClass.ARCHER   : 0,
-        CharacterClass.ASSASSIN :-3,
+        CharacterClass.ARCHER   : 3,
+        CharacterClass.ASSASSIN : 0,
         CharacterClass.CLERIC   : 9,
-        CharacterClass.KNIGHT   :-5,
+        CharacterClass.KNIGHT   : 1,
         CharacterClass.MAGE     : 7,
         CharacterClass.VALKYRIE : 10,
         CharacterClass.TESTCHAR : 0,
     },
     CharacterClass.ASSASSIN: {
-        CharacterClass.ARCHER   : 2,
-        CharacterClass.ASSASSIN : 0,
+        CharacterClass.ARCHER   : 5,
+        CharacterClass.ASSASSIN : 3,
         CharacterClass.CLERIC   : 10,
-        CharacterClass.KNIGHT   :-6,
-        CharacterClass.MAGE     : 4,
-        CharacterClass.VALKYRIE : 2,
+        CharacterClass.KNIGHT   : 1,
+        CharacterClass.MAGE     : 5,
+        CharacterClass.VALKYRIE : 3,
         CharacterClass.TESTCHAR : 0,
     },
     CharacterClass.CLERIC: {
-        CharacterClass.ARCHER   : 0,
-        CharacterClass.ASSASSIN :-9,
-        CharacterClass.CLERIC   : 0,
-        CharacterClass.KNIGHT   : 1,
-        CharacterClass.MAGE     : 0,
-        CharacterClass.VALKYRIE :-5,
+        CharacterClass.ARCHER   : 5,
+        CharacterClass.ASSASSIN : 1,
+        CharacterClass.CLERIC   : 5,
+        CharacterClass.KNIGHT   : 5,
+        CharacterClass.MAGE     : 5,
+        CharacterClass.VALKYRIE : 5,
         CharacterClass.TESTCHAR : 0,
     },
     CharacterClass.KNIGHT: {
         CharacterClass.ARCHER   : 0,
         CharacterClass.ASSASSIN : 8,
         CharacterClass.CLERIC   : 9,
-        CharacterClass.KNIGHT   :-3,
+        CharacterClass.KNIGHT   : 2,
         CharacterClass.MAGE     : 5,
         CharacterClass.VALKYRIE : 0,
         CharacterClass.TESTCHAR : 0,
     },
     CharacterClass.MAGE: {
-        CharacterClass.ARCHER   : 0,
+        CharacterClass.ARCHER   : 5,
         CharacterClass.ASSASSIN : 3,
-        CharacterClass.CLERIC   : 5,
-        CharacterClass.KNIGHT   : 7,
-        CharacterClass.MAGE     : 0,
-        CharacterClass.VALKYRIE : 0,
+        CharacterClass.CLERIC   : 6,
+        CharacterClass.KNIGHT   : 9,
+        CharacterClass.MAGE     : 4,
+        CharacterClass.VALKYRIE : 3,
         CharacterClass.TESTCHAR : 0,
     },
     CharacterClass.VALKYRIE: {
         CharacterClass.ARCHER   : 0,
-        CharacterClass.ASSASSIN :-3,
-        CharacterClass.CLERIC   : 3,
-        CharacterClass.KNIGHT   : 3,
+        CharacterClass.ASSASSIN : 2,
+        CharacterClass.CLERIC   : 5,
+        CharacterClass.KNIGHT   : 5,
         CharacterClass.MAGE     : 4,
-        CharacterClass.VALKYRIE : 0,
+        CharacterClass.VALKYRIE : 3,
         CharacterClass.TESTCHAR : 0,
     },
 }
@@ -305,17 +305,22 @@ def get_heuristic_board_value(game, player_color, enemy_threat_board):
     '''
     Returns a heuristically determined value of how good a given board situation is to a player.
     '''
+    value = Value()
+
     # Game has been determined
     if game.get_winner() is not None:
         # Current player won
         if game.get_winner() == player_color:
-            return  math.inf
+            value.add('Winner', math.inf)
+            return value
         # Current player lost
         else:
-            return -math.inf
+            value.add('Winner', -math.inf)
+            return value
         
     def distance_between_two_characters(first,second):
         return  abs(first.get_square()[0]-second.get_square()[0]) + abs(first.get_square()[1]-second.get_square()[1] ) 
+
 
     player = game.get_player(player_color)
     enemy = game.get_blue_player() if player_color == PlayerColor.RED else game.get_red_player()
@@ -323,54 +328,65 @@ def get_heuristic_board_value(game, player_color, enemy_threat_board):
     nof_player_characters = len(player.get_characters())
     nof_enemy_characters = len(enemy.get_characters())
 
-    value = 0
-
     # Base value for each character which is alive
-    value += sum([BASE_VALUES[char.type] for char in player.get_characters()])
-    value -= sum([BASE_VALUES[char.type] for char in enemy.get_characters()])
+    value += ( 'Characters', sum([BASE_VALUES[char.type] for char in player.get_characters()]) )
+    value -= ( 'En. characters', sum([BASE_VALUES[char.type] for char in enemy.get_characters()]) )
 
     # Points based on the current hp of the characters
-    value += 0.5*sum([BASE_VALUES[char.type] * char.get_hp() / char.get_maxhp() for char in player.get_characters()])
-    value -= 0.5*sum([BASE_VALUES[char.type] * char.get_hp() / char.get_maxhp() for char in enemy.get_characters()])
+    value += ( 'HP', 0.2*sum([BASE_VALUES[char.type] * char.get_hp() / char.get_maxhp() for char in player.get_characters()]) )
+    value -= ( 'EHP', 0.2*sum([BASE_VALUES[char.type] * char.get_hp() / char.get_maxhp() for char in enemy.get_characters()]) )
 
-    # For each character, -10 to +10 points based on how well it fares against remaining enemy characters
-    for player_char, enemy_char in itertools.product(player.get_characters(), enemy.get_characters()):
-        value += (nof_player_characters/nof_enemy_characters) * ADVANTAGES[player_char.type][enemy_char.type] / len(enemy.get_characters())
-        value -= (nof_enemy_characters/nof_player_characters) * ADVANTAGES[enemy_char.type][player_char.type] / len(player.get_characters())
+    value += ( 'Average HP', 0.1*sum([char.get_hp()/nof_player_characters for char in player.get_characters()]) )
+    value -= ( 'Average EHP', 0.1*sum([char.get_hp()/nof_enemy_characters for char in enemy.get_characters()]) )
 
     # For each char, drop the value if enemy can deal damage to this square
+    player_against_enemy_val = 0
+    enemy_against_player_val = 0
     for char in player.get_characters():
+        char_danger_val = 0
         prob_dmg = get_probable_enemy_damage(enemy_threat_board, char, char.get_square())
         max_dmg  = get_max_enemy_damage(enemy_threat_board, char, char.get_square())
-        value -= 0.1 * BASE_VALUES[char.type] * ( max(0, prob_dmg ) / char.get_maxhp() )
+
+        for enemy_char in enemy.get_characters():
+            if max_dmg < char.get_hp():
+                player_against_enemy_val += (nof_player_characters/nof_enemy_characters) * ( (char.get_hp()-prob_dmg) / char.get_maxhp() ) * ADVANTAGES[char.type][enemy_char.type] / len(enemy.get_characters())
+                enemy_against_player_val += (nof_enemy_characters/nof_player_characters) * ( enemy_char.get_hp() / enemy_char.get_maxhp() ) * ADVANTAGES[enemy_char.type][char.type] / len(player.get_characters())
+
+            elif prob_dmg < char.get_hp():
+                player_against_enemy_val += 0.5*(nof_player_characters/nof_enemy_characters) * ( (char.get_hp()-prob_dmg) / char.get_maxhp() ) * ADVANTAGES[char.type][enemy_char.type] / len(enemy.get_characters())
+                enemy_against_player_val += (nof_enemy_characters/nof_player_characters) * ( enemy_char.get_hp() / enemy_char.get_maxhp() ) * ADVANTAGES[enemy_char.type][char.type] / len(player.get_characters())
+
+            else:
+                enemy_against_player_val += (nof_enemy_characters/nof_player_characters) * ( enemy_char.get_hp() / enemy_char.get_maxhp() ) * ADVANTAGES[enemy_char.type][char.type] / len(player.get_characters())
+
+        char_danger_val -= 0.4 * BASE_VALUES[char.type] * ( max(0, prob_dmg ) / char.get_maxhp() )
         if prob_dmg > char.get_hp():
-            value -= 0.2 * BASE_VALUES[char.type]
+            char_danger_val -= 0.4 * BASE_VALUES[char.type]
         elif max_dmg > char.get_hp():
-            value -= 0.1 * BASE_VALUES[char.type]
+            char_danger_val -= 0.2 * BASE_VALUES[char.type]
+        value.add(f'{char.name} danger', char_danger_val)
+
+
+    # For each character, -10 to +10 points based on how well it fares against remaining enemy characters
+
+    #for player_char, enemy_char in itertools.product(player.get_characters(), enemy.get_characters()):
+    #    player_against_enemy_val += (nof_player_characters/nof_enemy_characters) * ADVANTAGES[player_char.type][enemy_char.type] / len(enemy.get_characters())
+    #    enemy_against_player_val += (nof_enemy_characters/nof_player_characters) * ADVANTAGES[enemy_char.type][player_char.type] / len(player.get_characters())
+    value += ('Player advantage', player_against_enemy_val)
+    value -= ('Enemy advantage', enemy_against_player_val)
+
+
 
     # Prefer positions where characters are close to each other
-    '''
-    if len(player.get_characters()) > 1:
-        for char in player.get_characters():
-            mean_distance_from_teammates = 0
-            for other_char in [ch for ch in player.get_characters() if ch != char]:
-                mean_distance_from_teammates += ( abs(char.get_square()[0] - other_char.get_square()[0] ) + abs(char.get_square()[1] - other_char.get_square()[1] ) ) / (len(player.get_characters()) - 1)
-            value += 10*(PREFER_ALLY_COMPANY[char.type]/10) * (1/mean_distance_from_teammates) / len(player.get_characters())
-    '''
-    value -= sum([PREFER_ALLY_COMPANY[char.type]*distance_between_two_characters(char, second_char) / (nof_player_characters**2) for char in player.get_characters() for second_char in player.get_characters() ])
+    value -= ('Allies close', 0.2 * sum([PREFER_ALLY_COMPANY[char.type]*distance_between_two_characters(char, second_char) / (nof_player_characters**2) for char in player.get_characters() for second_char in player.get_characters() ]) )
 
     # To avoid games where no player does anything, slightly favor positions where characters are closer to enemies
-    '''
-    for char in player.get_characters():
-        mean_distance_from_enemies = 0
-        for enemy_char in enemy.get_characters():
-            mean_distance_from_enemies += ( abs(char.get_square()[0] - enemy_char.get_square()[0] ) + abs(char.get_square()[1] - enemy_char.get_square()[1] ) ) / len(enemy.get_characters())
-        value += 5*(PREFER_ENEMY_COMPANY[char.type]/10) * (1/mean_distance_from_enemies) / len(player.get_characters())
-    '''
-    value -= (nof_player_characters/nof_enemy_characters) * sum([ PREFER_ENEMY_COMPANY[char.type]*distance_between_two_characters(char, enemy_char) / (nof_player_characters * nof_enemy_characters) for char in player.get_characters() for enemy_char in enemy.get_characters() ])
+    value -= ('Enemies close', 0.1 * (nof_player_characters/nof_enemy_characters) * sum([ PREFER_ENEMY_COMPANY[char.type]*distance_between_two_characters(char, enemy_char) / (nof_player_characters * nof_enemy_characters) for char in player.get_characters() for enemy_char in enemy.get_characters() ]) )
 
+    # Add random noise
+    value += ('Noise', random.gauss(0, 0.3))
 
-    return value + random.gauss(0, 0.5)
+    return value
 
 
 
@@ -435,9 +451,9 @@ def get_ai_move_2(game, player_color):
         value = get_heuristic_board_value(game_copy, player_color, enemy_threat_board)
         move.value = value
         print(move)
-        if value > best_value:
+        if value.get() > best_value:
             best_move = move
-            best_value = value
+            best_value = value.get()
 
     #print(f"copy: {copy_time}, apply: {apply_time}, heur: {heur_time}")
     print("Best:")
@@ -461,6 +477,8 @@ def get_max_enemy_damage(board, char, coordinates):
     all_attacks = board[coordinates[1]][coordinates[0]]
     char_attack = {}
     for enemy_char, attack in all_attacks:
+        if enemy_char.get_hp() == 0:
+            continue
         dmg = attack.calculate_max_damage(char)
         if enemy_char not in char_attack or dmg > char_attack[enemy_char]:
             char_attack[enemy_char] = dmg
@@ -470,6 +488,8 @@ def get_probable_enemy_damage(board, char, coordinates):
     all_attacks = board[coordinates[1]][coordinates[0]]
     char_attack = {}
     for enemy_char, attack in all_attacks:
+        if enemy_char.get_hp() == 0:
+            continue
         dmg = attack.calculate_probable_damage(char)
         if enemy_char not in char_attack or dmg > char_attack[enemy_char]:
             char_attack[enemy_char] = dmg
--- src/move.py
+++ src/move.py
@@ -3,18 +3,53 @@ Module contains class Move, which is a standardized way of conveying character t
 information.
 '''
 
-from typing import Any, SupportsIndex
+class Value:
+    '''
+    Class saves different aspects of the board heuristic values.
+    Useful for dewbugging.
+    '''
+    def __init__(self, title=None, val=None) -> None:
+        self.sections = []
+        if not title is None and not val is None:
+            self.add_section(title=title, val=val)
+
+    def add(self,title,val):
+        self.sections.append([title, val])
+
+    def get(self):
+        return round(sum([val for title, val in self.sections]), 2)
+    
+    def __iadd__(self, other):
+        if type(other) is float:
+            self.add('', other)
+        else:
+            self.add(other[0], other[1])
+        return self
+    
+    def __isub__(self, other):
+        if type(other) is float:
+            self.add('', -other)
+        else:
+            self.add(other[0], -other[1])
+        return self
+    
+    def __repr__(self) -> str:
+        return '\n\t' + ''.join([f"{title}: {round(val,2)}, " for title, val in self.sections]) + f"\n\tTotal: {self.get()}"
+
 
 
 class Move:
+    '''
+    Describes one action on the board: movement, and from there using an attack/skill/passing the turn.
+    '''
 
     def __init__(self, source_square, destination_square, target_square, action_type=None, action_id=None, value=0) -> None:
         
         self.source_square      = source_square
         self.destination_square = destination_square
         self.target_square      = target_square
-        self.action_type        = action_type
-        self.action_id          = action_id
+        self.action_type        = action_type           # 'a' for attack, 's' for skill, 'p' for passing the turn
+        self.action_id          = action_id             # Attack or Skill id, or None
         self.value              = value
 
     def __repr__(self) -> str:
